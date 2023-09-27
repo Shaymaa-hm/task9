2a53d85461fda8e570196c73e998295841cba4ac
Shaymaa-hm <82785354+Shaymaa-hm@users.noreply.github.com>
2023-09-27 11:03:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,24 @@
-function incrementWithQuerySelector(prod) {
-    if(prod==1)
-    {
-    var numbers = document.querySelector("#product1")
-    }
-    else if(prod==2)
-    {
-        var numbers = document.querySelector("#product2")
-    }
-    else if(prod==3)
-    {
-        var numbers = document.querySelector("#product3")
-    }
-    var integernum = parseInt(numbers.innerHTML)
-    if (integernum < 9) {
-        numbers.textContent = integernum + 1;
-    }
-}
+const plusBtns = document.querySelectorAll(".increment")
+plusBtns.forEach(btn => {
+    btn.addEventListener("click", function() {
+        const currentAmountElement = this.closest(".buttons").querySelector(".number")
+        const amountValue = parseInt(currentAmountElement.textContent)
+        if(amountValue<9)
+        {
+            currentAmountElement.textContent = amountValue + 1
+        }
+        
+    })
+})
 
-function decrementWithClosest(prod) {
-    
-    if(prod==1)
-    {
-        var productnum = document.getElementById("product1");
-    }
-    else if(prod==2)
-    {
-        var productnum = document.getElementById("product2");
-    }
-    else if(prod==3)
-    {
-        var productnum = document.getElementById("product3");
-    }
-    var integernum = parseInt(productnum.innerHTML)
-    
-    if (integernum > 1 && productnum.closest(".number")) {
-        productnum.textContent = integernum - 1;
-    }
-}
-// function increment() {
-//     var numbers = document.getElementsByClassName("number");
-//     var integernum = parseInt(numbers[0].innerHTML)
-//     if (integernum < 9) {
-//         numbers[0].textContent = integernum + 1;
-//     }
-// }
-
-// function decrement() {
-//     var numbers = document.getElementsByClassName("number");
-//     var integernum = parseInt(numbers[0].innerHTML)
-//     if (integernum > 1) {
-//         numbers[0].textContent = integernum - 1;
-//     }
-// }
-
+const minusBtns = document.querySelectorAll(".decrement")
+minusBtns.forEach(btn => {
+    btn.addEventListener("click", function() {
+        const currentAmountElement = this.closest(".buttons").querySelector(".number")
+        const amountValue = parseInt(currentAmountElement.textContent)
+        if(amountValue>1)
+        {
+            currentAmountElement.textContent = amountValue - 1
+        }
+    })
+})
